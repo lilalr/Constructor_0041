fd34c5351a43984f82f0cd191f0b8ef050fdf427
lilalr <gurennut@gmail.com>
2024-05-14 18:24:32 +0700

    menambah akses modifier public

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -5,3 +5,15 @@ class buku
 {
     string judul;
 
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this;   // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
+
