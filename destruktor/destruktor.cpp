b1a6b91f6a02eea5a16635ecb4eb4e0ee429e1d6
lilalr <gurennut@gmail.com>
2024-05-14 18:37:17 +0700

    konstruksi cetakData

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -30,3 +30,11 @@ angka::~angka()       // Destructor
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
